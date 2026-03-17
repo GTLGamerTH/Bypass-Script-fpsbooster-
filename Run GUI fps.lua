8e7ac384b54bb9500a6ffac2ec8e0be9d53b14ec
GTLGamerTH <sxnporcaex@gmail.com>
2026-03-18 02:33:46 +0700

    Run GUI Fps.lua

--- Run GUI fps.lua
+++ Run GUI fps.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/GTLGamerTH/-/blob/main/README.md"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GTLGamerTH/-/c63f86d43b228a8a0ef1298160148d5c3719b41d/README.md"))()
